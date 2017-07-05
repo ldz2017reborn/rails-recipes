2b63bf298cdf0bdc403bf54fa8e4f18e3d8a507c
Your Name <you@example.com>
2017-07-05 20:19:55 +0800

    rails 百宝箱123

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,9 @@ class Event < ApplicationRecord
  has_many :tickets, :dependent => :destroy
  has_many :registrations, :dependent => :destroy
  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
+ has_many :attachments, :class_name => "EventAttachment", :dependent => :destroy
+ accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
  STATUS = ["draft", "public", "private"]
  validates_inclusion_of :status, :in => STATUS
  validates_presence_of :name, :friendly_id
